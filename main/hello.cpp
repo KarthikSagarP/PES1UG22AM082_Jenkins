3446d08f9c7b715e4ef8094bbbd79a0dd14298cc
KarthikSagarP <127867230+KarthikSagarP@users.noreply.github.com>
2025-03-14 16:55:13 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -3,7 +3,7 @@
 using namespace std;
 
 int main(void) {
-  cout << "Hello, Planet\n";
+  cout << "Hello, Wrold\n";
   cout << "Hello, Jenkins\n";
   cout << "I have successfully built and run this  \n";
   return 0;
